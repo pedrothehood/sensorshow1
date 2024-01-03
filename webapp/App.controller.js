a2ee37333ce6a145535890c29fbc3fcc0543c768
Peter Gloor <peter.gloor2@bluewin.ch>
2024-01-03 17:59:48 +0100

    Abfangen fehlerhafte/fehlerhafte Sensordaten

--- webapp/App.controller.js
+++ webapp/App.controller.js
@@ -5,20 +5,24 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("Quickstart.App", {
-		onPress : function () {
-				
-			dweetio.get_latest_dweet_for("pedrothehoodesp8266", function(err, dweet){
+		onPress: function () {
 
-    var dweet = dweet[0]; // Dweet is always an array of 1
+			dweetio.get_latest_dweet_for("pedrothehoodesp8266", function (err, dweet) {
 
-    console.log(dweet.thing); // The generated name
-    console.log(dweet.content); // The content of the dweet
-    console.log(dweet.created); // The create date of the dweet
-    MessageToast.show("Temperatur: " + dweet.content.temperature + "C Feuchtigkeit: " +  dweet.content.humidity + "%");
-	
-});
-  
-		//	MessageToast.show("Temperatur: " + dweet.content.temperature + "C Feuchtigkeit: " +  dweet.content.humidity + "%");
+				var dweet = dweet[0]; // Dweet is always an array of 1
+				try {
+					console.log(dweet.thing); // The generated name
+					console.log(dweet.content); // The content of the dweet
+					console.log(dweet.created); // The create date of the dweet
+					MessageToast.show("Temperatur: " + dweet.content.temperature + "C Feuchtigkeit: " + dweet.content.humidity + "%");
+
+				} catch (error) {
+					MessageToast.show("Keine Daten gefunden (Sensor hat mind. 24 Std. nicht gesendet)")
+				}
+			}
+			);
+
+			//	MessageToast.show("Temperatur: " + dweet.content.temperature + "C Feuchtigkeit: " +  dweet.content.humidity + "%");
 		}
 	});
 
